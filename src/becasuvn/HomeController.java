ad3e1e0847e5b508d9b01caaa3244754acfd959c
AlguienMore <hurtadojosmanuel@gmail.com>
2020-05-18 15:56:31 -0500

    Configuracion de sesion.

--- src/becasuvn/HomeController.java
+++ src/becasuvn/HomeController.java
@@ -11,6 +11,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import java.sql.Connection;
 
 /**
  * FXML Controller class
@@ -25,17 +26,29 @@ public class HomeController implements Initializable {
     @FXML
     JFXPasswordField passwtxt;
     
+    ConexionMySQL session = new ConexionMySQL(); 
+    
     boolean VerifyUser(){
         if(usertxt.getText().isEmpty() || passwtxt.getText().isEmpty()){
+            System.out.println("Campos vacios");
             return false;
         }else{
+            System.out.println("We good to go");
             return true;
         }
     }
     
     @FXML
     void logIn(){
-        
+        if (this.VerifyUser()){
+            Connection conn = session.config("test", usertxt.getText(), passwtxt.getText());
+            if (conn != null){
+                System.out.println("Sesion lista");
+            }else{
+                System.out.println("Credenciales Invalidas");
+                
+            }
+        }
     }
     
     @Override
